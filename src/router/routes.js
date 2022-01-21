d41d1163d1289a9f3a1b2b14d1e04c5f924d43ea
shenlian8 <shenlian@gmx.de>
2022-01-21 16:07:52 +0100

    clean up quasar project

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,7 @@
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    component: () => import('pages/Index.vue')
   },
 
   // Always leave this as last one,
