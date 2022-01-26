5586b4cc6c85d8675a5b403dd29afd52d6a54d33
shenlian8 <shenlian@gmx.de>
2022-01-26 16:41:59 +0100

    show elements overlay

--- src/components/ScreenView.vue
+++ src/components/ScreenView.vue
@@ -1,8 +1,33 @@
 <template>
-  <img id="screen-image" :src="'data:image/png;base64, ' + imgData.value" style="width: 100%; object-fit: scale-down;"/>
-
+  <img id="screen-image" :src="'data:image/png;base64, ' + imgData.value" style="width: 100%; object-fit: scale-down;" alt=""/>
+  <div id="element-overlay" style="width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;">
+    <div v-for="element in elements"
+         :id="element.id"
+         :key="element.id"
+         :style="element.boundStyle"
+         v-on:click="showAttr"
+         class="element-frame">
+    </div>
+  </div>
 </template>
-
+<style>
+.element-frame {
+  position: absolute;
+  border: 1px dashed rgba(0,255,0,.5);
+}
+.element-frame:hover {
+  border: 1px solid rgba(0,255,0,1);
+}
+.element-frame-selected {
+  position: absolute;
+  border: 1px solid green;
+  background-color: rgba(154,205,50,.3);
+}
+</style>
 <script>
 const base64Jpeg = {"sessionId":"","value":"R0lGODlhAQABAAAAACwAAAAAAQABAAA="};
 
@@ -10,12 +35,64 @@ export default {
   name: "ScreenView",
   data() {
     return {
-      imgData: base64Jpeg
+      imgData: base64Jpeg,
+      elements: [],
+      oriElements: [],
+      selectedId: null
     }
   },
-  mounted: function() {
+  methods: {
+    renderElements: function() {
+      this.elements = JSON.parse(JSON.stringify(this.oriElements));
+
+      const imgClientWidth = document.getElementById("screen-image").clientWidth;
+      let imgNaturalWidth = document.getElementById("screen-image").naturalWidth;
+
+      if(this.elements.length > 0) {
+        if("width" in this.elements[0]) {
+          imgNaturalWidth = this.elements[0].width
+        }
+      }
+      for(let index = 0; index < this.elements.length; index ++) {
+        let oneElement;
+        oneElement = this.elements[index];
+        oneElement.left = oneElement.left * imgClientWidth / imgNaturalWidth;
+        oneElement.top = oneElement.top * imgClientWidth / imgNaturalWidth;
+        oneElement.width = oneElement.width * imgClientWidth / imgNaturalWidth - 2;
+        oneElement.height = oneElement.height * imgClientWidth / imgNaturalWidth - 2;
+        oneElement.boundStyle = "left:" + oneElement.left + "px;top:" + oneElement.top + "px;width:" + oneElement.width + "px;height:" + oneElement.height + "px;";
+      }
+    },
+    showAttr: function(event) {
+      this.$root.elementClicked = event.currentTarget.id;
+    },
+    markElement: function(id) {
+      if(this.selectedId != null) {
+        if(document.getElementById(this.selectedId) != null) {
+          document.getElementById(this.selectedId).className = "element-frame";
+        }
+      }
+      if(document.getElementById(id) != null) {
+        document.getElementById(id).className = "element-frame-selected";
+      }
+      this.selectedId = id;
+    }
+  },
+  created: function() {
+    window.addEventListener("resize", this.renderElements);
+
     window.myApi.receive("updateImage", (data) => {
       this.imgData = data;
+    });
+
+    window.myApi.receive("updateElementView", (data) => {
+      // console.log(data);
+      this.$root.elementClicked = null;
+
+      this.oriElements = data;
+      this.renderElements();
+
+      // root.$refs.attrTable.oriElements = data;
     });
   }
 }
