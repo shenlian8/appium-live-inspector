ae878835f991166c05eed8837b1f87b40668d733
shenlian8 <shenlian@gmx.de>
2022-01-28 15:56:40 +0100

    create element details component

---
+++ src/components/ElementDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+export default {
+  name: "ElementDetails",
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  created: function() {
+    window.myApi.receive("updateElementView", (data) => {
+      //this.oriElements = data;
+    });
+  }
+}
+</script>
