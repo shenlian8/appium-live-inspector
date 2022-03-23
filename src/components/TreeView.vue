65558613ac3ed451f240f306c12d8f310f7a0d3b
shenlian8 <shenlian@gmx.de>
2022-03-23 17:57:24 +0100

    show tree data

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,28 +1,42 @@
 <template>
   <q-scroll-area class="col q-pa-sm">
-
+    <q-tree
+      :nodes="treeData"
+      dense
+      node-key="id"
+      default-expand-all
+      :duration="0"
+      ref="tree"
+      :selected="selected"
+      @update:selected="triggerNodeSelected"
+    />
   </q-scroll-area>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'Treeview',
-  setup () {
-    return {
-    }
-  },
   data() {
     return {
+      treeData: [],
+      selected: []
     }
   },
   methods: {
+    triggerNodeSelected: function(event) {
+      window.mitt.emit("Element selected", event);
+    },
   },
   created: function() {
-    window.myApi.receive("updateElementView", (data) => {
+    window.myApi.receive("updateTreeView", (data) => {
+      this.treeData = data;
+      this.$nextTick(()=>{this.$refs.tree.expandAll()})
     });
 
     window.mitt.on('Element selected', (elementId) => {
+
     });
   }
     };
